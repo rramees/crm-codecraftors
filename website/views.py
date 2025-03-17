96b6255f1b0b85f8b04beb7bbc07d4e0f4eaf5a2
rramees <ramees8614@gmail.com>
2025-03-17 16:15:17 +0530

    register completed

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -24,3 +25,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html' ,{'form':form})
